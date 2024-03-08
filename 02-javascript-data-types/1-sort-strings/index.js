0bb8e87a9f065b998b01bf95e6cfad480115b63c
Alyona Сrainova <alyonacrainova@icloud.com>
2024-03-08 20:30:00 +0300

    reverse func exclusion

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,7 +4,7 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
+
 export function sortStrings(arr, param = 'asc') {
-    let sortedArray = arr.slice( ).sort( (a, b) => a.localeCompare(b, 'ru-RU-u-kf-upper' ) );
-    return param == 'asc' ?  sortedArray : sortedArray.reverse( );
+    return arr.slice( ).sort( (a, b) => a.localeCompare(b, 'ru-RU-u-kf-upper' ) * ( param == 'asc' ? 1 : -1 ) );
 }
