f6d5fd6bdaf55649d11b8b09e8ffe76f4944498e
Alyona Сrainova <alyonacrainova@icloud.com>
2024-03-08 22:15:59 +0300

    omit func

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+    let newObj = { };
+    Object.entries(obj).forEach( ([key, value]) => {
+        if (!fields.includes(key)) {
+            newObj[key] = value;
+        } 
+    } );
+    return newObj;
 };
+
