26a34793d32ac73727ed968079889baa3f18187d
Alyona Сrainova <alyonacrainova@icloud.com>
2024-03-17 19:52:00 +0300

    5-1 task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,63 @@
 export default class NotificationMessage {
+    element;
+    static lastInstance = null;
+
+    constructor( message = '', 
+                { type = '',
+                  duration = 0,
+                } = {}) {
+        this.message = message;
+        this.type = type;
+        this.duration = duration;
+        this.element = this.createElement(this.createTemplate());
+    }
+
+    show(container = document.body) {
+        if (NotificationMessage.lastInstance) {
+            NotificationMessage.lastInstance.destroy();
+        }
+        NotificationMessage.lastInstance = this;
+        
+        this.timerId = setTimeout(() => {
+            this.destroy();
+        }, this.duration);
+
+        container.appendChild(this.element);
+    }
+
+    hide() {
+        //console.log('hide')
+    }
+    
+    remove() {
+        //console.log('remove')
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+        clearTimeout(this.timerId);
+    }
+
+    createTemplate(){
+        return `<body>
+                    <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+                        <div class="timer"></div>
+                        <div class="inner-wrapper">
+                            <div class="notification-header">${this.type}</div>
+                            <div class="notification-body">${this.message}</div>
+                        </div>
+                    </div>
+                 </body>`;
+                
+    }
+
+    createElement(template){
+        const element = document.createElement('div');
+
+        element.innerHTML = template;
+
+        return element.firstElementChild;
+    }
 
 }
